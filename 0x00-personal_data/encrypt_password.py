450a0bc1892337e26540fba3bb066fc745e5b84c
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-15 18:07:14 +0200

    Password encryption

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,7 +7,7 @@ Encrypt a password
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password -> str) -> bytes:
     """ Function that hashes a password """
     password = password.encode()
     return bcrypt.hashpw(password, bcrypt.gensalt())
