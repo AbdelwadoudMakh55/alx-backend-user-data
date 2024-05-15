a8ce78da06fbc8dc201b9746daaf92ec53353cbc
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-15 21:05:32 +0200

    Log info

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python3
-"""
-Module that has many functions that deals with personal data.
-"""
-import logging
+""" doc doc doc """
 import re
 from typing import List
+import logging
+import os
+import mysql.connector
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """Obfuscate fields from a log string """
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """doc doc doc"""
     for field in fields:
-        match = re.search(rf'{field}=(.*?){separator}', message)
-        if match:
-            message = re.sub(match.group(1), redaction, message, 1)
+        regex = f"{field}=[^{separator}]*"
+        message = re.sub(regex, f"{field}={redaction}", message)
     return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class """
+    """doc doc doc"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
-        """ Init function """
+    def __init__(self, fields: List[str]):
+        """doc doc doc"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Format the LogRecord """
-        record.msg = filter_datum(self.fields, self.REDACTION,
-                                  record.msg, self.SEPARATOR)
-        return super().format(record)
+        """doc doc doc"""
+        org = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, org, self.SEPARATOR)
---
+++ 0x00-personal_data/filtered_logger_.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Module that has many functions that deals with personal data.
+"""
+
+
+import logging
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Obfuscate fields from a log string """
+    for field in fields:
+        match = re.search(rf'{field}=(.*?){separator}', message)
+        if match:
+            message = re.sub(match.group(1), redaction, message, 1)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        """ Init function """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the LogRecord """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super().format(record)
