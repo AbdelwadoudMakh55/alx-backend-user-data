33b7bb965fdcbfb2edf88e244f36fe6f6aa07b84
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-16 14:43:31 +0200

    Fix regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,7 +19,7 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Obfuscate fields from a log string """
     for field in fields:
-        match = re.search(f'{field}=(.*?){separator}', message)
+        match = re.search(f'{field}=([^{separator}]*)', message)
         message = re.sub(re.escape(match.group(1)), redaction, message)
     return message
 
