bc7bb003d780517a478684f08e8aaf6afcdee54e
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-20 15:40:07 +0200

    Decode Base64 Auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,17 @@ class BasicAuth(Auth):
            authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str
+    ) -> str:
+        """ Decode base64 from auth header """
+        if base64_authorization_header is None or \
+           type(base64_authorization_header) != str:
+            return None
+        try:
+            decoded_str = base64.b64decode(base64_authorization_header)
+            return decoded_str.decode('utf-8')
+        except Exception:
+            return None
