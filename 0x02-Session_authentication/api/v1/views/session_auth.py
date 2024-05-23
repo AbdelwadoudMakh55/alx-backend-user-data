dd775578ff8f6c6a380331c93ad4ffaa5ec61545
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-23 00:49:20 +0000

    Close session (logout)

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -34,7 +34,10 @@ def login():
     response.set_cookie(cookie_session_id, session_id)
     return response
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
 def delete_session():
     """ Route for deleting session """
     from api.v1.app import auth
