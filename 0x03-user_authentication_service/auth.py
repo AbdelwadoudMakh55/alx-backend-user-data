fcd012cb0f43d306d1a4180cc0616a69723a2350
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-28 23:19:36 +0100

    Validate credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,3 +32,13 @@ class Auth:
                 raise ValueError(f"User {email} already exists")
             hashed_pwd = _hash_password(password)
             return self._db.add_user(email, hashed_pwd)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode(), user.hashed_password):
+                return True
+            return False
+        except Exception:
+            return False
