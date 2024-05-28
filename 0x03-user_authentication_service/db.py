50c1bc2d1148dc2db16b4b393be4b408318e519d
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-28 13:57:59 +0100

    Search for user using kwargs

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 
 
@@ -36,3 +37,14 @@ class DB:
         session.add(new_user)
         session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """Find a user """
+        session = self._session
+        try:
+            user = session.query(User).filter_by(**kwargs).first()
+        except InvalidRequestError as e:
+            raise e
+        if not user:
+            raise (NoResultFound)
+        return user
