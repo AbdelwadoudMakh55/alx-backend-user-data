169710406339676edf932b1c95c81d5289ba8d3a
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-29 14:00:32 +0100

    Reset password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,6 +88,6 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
             self._db.update_user(user.id, hashed_password=hashed_password)
-            self._db.update_user(user.id, reset_token=reset_token)
+            self._db.update_user(user.id, reset_token=None)
         except Exception:
             raise ValueError
