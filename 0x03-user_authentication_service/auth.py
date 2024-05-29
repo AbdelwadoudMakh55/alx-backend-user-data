96cafc19735cfb35e842fddc17b311010fbcac6d
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-29 13:47:30 +0100

    Generate password reset token for a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,3 +71,12 @@ class Auth:
     def destroy_session(self, user_id: str) -> None:
         """Destroy a session"""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset token for a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+        except Exception:
+            raise ValueError
