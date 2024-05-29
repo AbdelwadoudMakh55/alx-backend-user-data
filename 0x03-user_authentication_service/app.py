bfc4e57aed90ff2729488d673649cb5bde81b311
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-29 13:39:24 +0100

    Destroy session and logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -44,6 +44,8 @@ def login():
 def logout():
     """Logout"""
     session_id = request.cookies.get("session_id")
+    if session_id is None:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
