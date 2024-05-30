0ccd93634b03df221f353f0cee57bce3abf8c81c
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-30 19:22:46 +0100

    End-to-end integration test of the authentication system

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -78,7 +78,7 @@ def get_reset_password():
         abort(403)
 
 
-@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+@app.route("/reset_password", methods=["PUT"])
 def update_password():
     """Update a user's password"""
     email = request.form.get("email")
